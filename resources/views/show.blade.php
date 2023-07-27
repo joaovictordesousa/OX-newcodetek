208c998efcb431fd02b8edc5acd93eda56b83be5
joaovictodesousa <joaovictorsr26@gmail.com>
2023-07-27 14:58:57 -0300

    melhoramento da tela

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -1,80 +1,41 @@
 @include('cabecalho.cabecalho')
 
-<div class="">
-    <div class="card">
-        <div class="card-body text-center">
+    <div class="card" style="width: 50%; margin: 4em 10em;">
+        <div class="card-body text-center" >
             <h4 class="card-title ">Ficha completa</h4>
             <div class="container">
                 <div class="row">
                     <div class="col">
-                        <table class="table">
-                            <thead>
-                                <tr>
-                                    <th scope="col">Nome ou
-                                        Nº da Vaca</th>
-                                </tr>
-                            </thead>
+                        <table class="table show_show">
                             <tbody>
-                                <tr>
-                                    <td>{{ $sheet->tag }}</td>
-                                </tr>
+                              <tr>
+                                <td><b>Nome ou Nº da Vaca:</b></td>
+                                <td>{{ $sheet->tag }}</td>
+                              </tr>
+                              <tr>
+                                <td><b>Nome ou Nº do Touro:</b></td>
+                                <td>{{ $sheet->bull_tag }}</td>
+                              </tr>
+                              <tr>
+                                <td><b>Nome do Inseminador:</b></td>
+                                <td>{{ $sheet->inseminator_name }}</td>
+                              </tr>
+                              <tr>
+                                <td><b>Data da inseminação:</b></td>
+                                <td>{{ $sheet->insemination_date }}</td>
+                              </tr>
+                              <tr>
+                                <td><b>Data prevista para o nascimento:</b></td>
+                                <td>{{ $sheet->birth_prediction }}</td>
+                              </tr>
+                              <tr>
+                
                             </tbody>
-                        </table>
-                        <table class="table">
-                            <thead>
-                                <tr>
-                                    <th scope="col">Nome ou Nº
-                                        do Touro</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <td>{{ $sheet->bull_tag }}</td>
-                                </tr>
-                            </tbody>
-                        </table>
-                        <table class="table">
-                            <thead>
-                                <tr>
-                                    <th scope="col">Nome do
-                                        Inseminador</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <td>{{ $sheet->inseminator_name }}</td>
-                                </tr>
-                            </tbody>
-                        </table>
-                        <table class="table">
-                            <thead>
-                                <tr>
-                                    <th scope="col">Data da inseminação</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <td>{{ $sheet->insemination_date }}</td>
-                                </tr>
-                            </tbody>
-                        </table>
-                        <table class="table">
-                            <thead>
-                                <tr>
-                                    <th scope="col">Data prevista para o nascimento</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <td>{{ $sheet->birth_prediction }}</td>
-                                </tr>
-                            </tbody>
-                        </table>
+                          </table>
                     </div>
                 </div>
-            </div>
-
-            <a href="#" class="btn btn-primary">Button</a>
+                <br><br>
+            <a href="{{ route('sheet.index') }}" class="btn btn-primary" style="padding: 10px 2em">Voltar</a>
         </div>
     </div>
 </div>
